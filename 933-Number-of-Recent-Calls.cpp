ca85599e691a76ad6205222a7913d83521f10b34
Ramanchi Rishi Sai Teja <102644611+xenon1919@users.noreply.github.com>
2024-12-20 21:46:27 +0530

    [933] [Time Beats: 90.61%] [Memory Beats: 25.92%] - LeetPush

---
+++ 933-Number-of-Recent-Calls.cpp
@@ -0,0 +1,22 @@
+class RecentCounter {
+private: 
+    queue<int> requests;
+public:
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        requests.push(t);
+        while(!requests.empty() && requests.front() < t - 3000) {
+            requests.pop();
+        }
+        return requests.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
