1112498f244e58eda222a20832c106de4cc38408
Ramanchi Rishi Sai Teja <102644611+xenon1919@users.noreply.github.com>
2024-12-21 18:47:42 +0530

    [2215] [Time Beats: 55.83%] [Memory Beats: 45.03%] - LeetPush

---
+++ 2215-Find-the-Difference-of-Two-Arrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> set1(nums1.begin(), nums1.end());
+        unordered_set<int> set2(nums2.begin(), nums2.end());
+
+        vector<int> diff1;
+        for(auto num : set1) {
+            if(set2.find(num) == set2.end()) {
+                diff1.push_back(num);
+            }
+        }
+        vector<int> diff2;
+        for(auto num : set2) {
+            if(set1.find(num) == set1.end()) {
+                diff2.push_back(num);
+            }
+            
+        }
+        return {diff1, diff2};
+    }
+
+};
