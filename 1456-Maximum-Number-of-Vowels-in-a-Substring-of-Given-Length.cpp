caa120d34616d95a7719478eb47eacf4fde55303
Ramanchi Rishi Sai Teja <102644611+xenon1919@users.noreply.github.com>
2024-12-23 23:46:22 +0530

    [1456] [Time Beats: 89.65%] [Memory Beats: 18.06%] - LeetPush

---
+++ 1456-Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+        auto isVowel = [](char c) {
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        };
+
+        int maxVowels = 0; 
+        int currentVowels = 0; 
+
+    
+        for (int i = 0; i < k; i++) {
+            if (isVowel(s[i])) {
+                currentVowels++;
+            }
+        }
+        maxVowels = currentVowels;
+
+   
+        for (int i = k; i < s.length(); i++) {
+            if (isVowel(s[i - k])) {
+                currentVowels--; 
+            }
+            if (isVowel(s[i])) {
+                currentVowels++; 
+            }
+            maxVowels = max(maxVowels, currentVowels);
+        }
+
+        return maxVowels;
+    }
+};
