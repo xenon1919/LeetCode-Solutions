029d97d9f31334d8f4687ef9c9e319a73233e481
Ramanchi Rishi Sai Teja <102644611+xenon1919@users.noreply.github.com>
2024-12-27 20:17:43 +0530

    [1332] [Time Beats: 100.00%] [Memory Beats: 55.10%] - LeetPush

---
+++ 1332-Remove-Palindromic-Subsequences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while(left < right) {
+            if(s[left] != s[right]) {
+                return 2;
+            }
+
+            left++;
+            right--;
+        }
+
+        return 1;
+        
+    }
+};
