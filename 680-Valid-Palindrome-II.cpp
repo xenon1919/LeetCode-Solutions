17dd151dda37e323c1b2e23e1162629656adf769
Ramanchi Rishi Sai Teja <102644611+xenon1919@users.noreply.github.com>
2025-01-15 11:07:51 +0530

    [680] [Time Beats: 62.20%] [Memory Beats: 95.25%] - LeetPush

---
+++ 680-Valid-Palindrome-II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(const string &s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false; 
+            }
+            left++;
+            right--;
+        }
+        return true; 
+    }
+
+    bool validPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true; 
+    }
+};
