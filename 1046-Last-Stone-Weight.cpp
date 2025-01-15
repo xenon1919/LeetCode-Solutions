6540392a7a45eec07b0e7925139cb01ae4559d78
Ramanchi Rishi Sai Teja <102644611+xenon1919@users.noreply.github.com>
2025-01-15 11:06:46 +0530

    [1046] [Time Beats: 100.00%] [Memory Beats: 94.21%] - LeetPush

---
+++ 1046-Last-Stone-Weight.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap(stones.begin(), stones.end());
+
+        while(maxHeap.size() > 1) {
+            int stone1 = maxHeap.top();
+            maxHeap.pop();
+            int stone2 = maxHeap.top();
+            maxHeap.pop();
+
+            if(stone1 != stone2) {
+                maxHeap.push(stone1 - stone2);
+            }
+        }
+
+        return maxHeap.empty() ? 0 : maxHeap.top();
+        
+    }
+};
