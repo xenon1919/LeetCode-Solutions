18528798031373008aa9761fe31cdaef01f6ba2b
Ramanchi Rishi Sai Teja <102644611+xenon1919@users.noreply.github.com>
2025-01-15 11:04:50 +0530

    [16] [Time Beats: 94.56%] [Memory Beats: 82.57%] - LeetPush

---
+++ 16-3Sum-Closest.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int closestSum = nums[0] + nums[1] + nums[3];
+
+        for(int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while(left < right) {
+                int currentSum = nums[i] + nums[left] + nums[right];
+
+                if(abs(currentSum - target) < abs(closestSum - target)) {
+                    closestSum = currentSum;
+                }
+
+                if(currentSum < target) {
+                    left++;
+                }
+
+                else if(currentSum > target) {
+                    right--;
+                }
+
+                else {
+                    return currentSum;
+                }
+            }
+        }
+
+        return closestSum;
+        
+    }
+};
