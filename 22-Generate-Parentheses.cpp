d0c9a64886f4a128f30c073565069c6af04a7216
Ramanchi Rishi Sai Teja <102644611+xenon1919@users.noreply.github.com>
2025-01-31 15:26:43 +0530

    [22] [Time Beats: 85.71%] [Memory Beats: 62.65%] - LeetPush

---
+++ 22-Generate-Parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void backtrack(vector<string> &result, string current, int open, int close, int n) {
+        if (current.length() == 2 * n) {
+            result.push_back(current);
+            return;
+        }
+        
+        if (open < n) {
+            backtrack(result, current + "(", open + 1, close, n);
+        }
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, n);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+};
