eb179d644c07dc924d99ac5f4419f10e8f191867
Ramanchi Rishi Sai Teja <102644611+xenon1919@users.noreply.github.com>
2025-03-23 12:39:32 +0530

    [2523] [Time Beats: 31.72%] [Memory Beats: 22.66%] - LeetPush

---
+++ Closest-Prime-Numbers-in-Range.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> closestPrimes(int left, int right) {
+        const int N = 1e6 + 1;
+        vector<bool> isPrime(N, true);
+        isPrime[0] = isPrime[1] = false;
+
+       
+        for (int i = 2; i * i < N; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < N; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+    
+        vector<int> primes;
+        for (int i = left; i <= right; i++) {
+            if (isPrime[i]) primes.push_back(i);
+        }
+
+      
+        if (primes.size() < 2) return {-1, -1};
+
+ 
+        int minDiff = INT_MAX;
+        vector<int> ans(2, -1);
+
+        for (int i = 1; i < primes.size(); i++) {
+            int diff = primes[i] - primes[i - 1];
+            if (diff < minDiff) {
+                minDiff = diff;
+                ans = {primes[i - 1], primes[i]};
+            }
+        }
+
+        return ans;
+    }
+};
