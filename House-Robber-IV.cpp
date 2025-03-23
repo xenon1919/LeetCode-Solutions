1a5d0574386e88bf85dd0caf790ca9c9228f66c5
Ramanchi Rishi Sai Teja <102644611+xenon1919@users.noreply.github.com>
2025-03-23 12:40:48 +0530

    [2560] [Time Beats: 94.14%] [Memory Beats: 33.05%] - LeetPush

---
+++ House-Robber-IV.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canRob(vector<int>& nums, int k, int capability) {
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] <= capability) { 
+                count++;
+                i++;  
+            }
+        }
+        return count >= k;
+    }
+
+    int minCapability(vector<int>& nums, int k) {
+        int left = *min_element(nums.begin(), nums.end());
+        int right = *max_element(nums.begin(), nums.end());
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canRob(nums, k, mid)) {
+                right = mid;  
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
