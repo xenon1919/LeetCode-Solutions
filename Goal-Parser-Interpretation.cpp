1401d40c0ae6517f285e602a7fe8a712a9f1c12b
Ramanchi Rishi Sai Teja <102644611+xenon1919@users.noreply.github.com>
2025-04-06 13:05:04 +0530

    [1678] [Time Beats: 9.37%] [Memory Beats: 74.44%] - LeetPush

---
+++ Goal-Parser-Interpretation.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string result;
+        for (int i = 0; i < command.length(); i++) {
+            if (command[i] == 'G') 
+                result += "G";                 
+            else if (command[i] == '(' && command[i+1] == ')') {
+                result += "o";                
+                i++;                          
+            }
+            else {
+                result += "al";               
+                i += 3;                      
+            }
+        }
+        return result;
+    }
+};
