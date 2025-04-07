1d89528d575007a82d11e42e1ef24e5e88c3d4ed
Ramanchi Rishi Sai Teja <102644611+xenon1919@users.noreply.github.com>
2025-04-07 12:42:50 +0530

    [1662] [Time Beats: 100.00%] [Memory Beats: 15.58%] - LeetPush

---
+++ Check-If-Two-String-Arrays-are-Equivalent.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+   bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+    string s1, s2;
+    for (string w : word1) s1 += w;
+    for (string w : word2) s2 += w;
+    return s1 == s2;
+}
+
+};
